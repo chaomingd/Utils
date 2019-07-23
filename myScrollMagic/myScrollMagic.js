16d31fb2b1c73fcc306e7e072d8238fa4ef54e00
leon.zeng <leon@fpi.cc>
2019-07-23 17:39:09 +0800

    update

--- myScrollMagic/myScrollMagic.js
+++ myScrollMagic/myScrollMagic.js
@@ -1,31 +1,260 @@
+import EventEmitter from 'events'
+import { _inherits,addEvent,getElRect } from './util';
+import styler from 'stylefire'
+
+_inherits(Scene,EventEmitter)
+
 function ScrollMagic() {
-	if((this instanceof ScrollMagic)) {
+	if ((this instanceof ScrollMagic)) {
 		throw new TypeError("Cannot new a base function");
 	}
 }
+ScrollMagic.Controller = Controller;
+ScrollMagic.Scene = Scene;
 
 function Controller() {
-	this._Scenes = [];
+	this._containers=[];
+	this._scens=[];
+}
+Controller.prototype.addScrollEvent=function (containerElement,fn) {
+	return addEvent(containerElement,'scroll',fn)
 }
 
 function Scene(options) {
-	let state = {
-		scrollState: 'leave',
+	this.minBoundaryValue=0;
+	this._lastRectValue=0;
+	this._scrollDestory=null;
+	this._parentNode = null;
+	this._parentNodeStyler = null;
+	this._isPin = false;
+	this._showUpdatePin = true; // 是否更新pin
+	this._pinLeft = 0; // fixed定位时的left值
+	this.state={
+		scrollState: '',
 		scrollDirection: '',
 		progress: 0,
 	}
-	let defaultOptions = {
+	let defaultOptions={
 		duration: 0,
 		offset: 0,
 		triggerHook: 0,
+		direction: 'vertical',
+		alwaysGetElementPostion: false,
 	}
-	let Op = Object.assign(defaultOptions,options);
-	if(!Op.triggerElement) {
+	var Op=this.Op=Object.assign(defaultOptions,options);
+	if (!Op.triggerElement) {
 		throw new Error('triggerElement must be a node element');
 	}
-	if(Op.triggerElement.nodeType !== 1) {
+	if (Op.triggerElement.nodeType!==1) {
 		throw new Error('triggerElement must be node element');
+	}
+	this.triggerElement=Op.triggerElement;
+	this.setBoundaryValue();
+	this.update=this.update.bind(this);
+	this.addEvent();
+}
+
+Scene.prototype.addEvent = function() {
+	this.on('enter',this._enter,this);
+	this.on('leave',this._leave,this);
+}
+
+Scene.prototype.update=function () {
+	var triggerElementRect=getElRect(this.triggerElement);
+	var rectValue=this.Op.direction==='vertical'? triggerElementRect.top:triggerElementRect.left;
+	if (this._lastRectValue<rectValue) {
+		this.state.scrollDirection='down';
+	} else {
+		this.state.scrollDirection='up';
+	}
+	var minBoundaryValue=this.minBoundaryValue;
+	if (this.Op.duration===0) {  // 两个区间 < minBoundaryValue or > minBoundaryValue
+		if (rectValue<=minBoundaryValue) {
+			if (this.state.scrollState!=='enter') {
+				this.state.scrollState='enter'
+				this.state.progress=1;
+				this._showUpdatePin = true;
+				this.emit('enter');
+				this.emit('progress',this.statetoEvent())
+			}
+		} else {
+			if (this.state.scrollState==='enter') {
+				this.state.scrollState='leave';
+				this.state.progress=0;
+				this._showUpdatePin = true;
+				this.emit('leave');
+				this.emit('progress',this.statetoEvent())
+			}
+		}
+	} else {
+		if ((rectValue<=minBoundaryValue)&&(rectValue>=this.maxBoundaryValue)) {
+			// 三个区间 < minBoundaryValue or minBoundaryValue~maxBoundaryValue or > maxBoundaryValue
+			this.state.progress=(minBoundaryValue-rectValue)/this.Op.duration;
+			if (this.state.scrollState!=='enter') {
+				this.state.scrollState='enter'
+				this._showUpdatePin = true;
+				this.emit('enter');
+			}
+			this.emit('progress',this.statetoEvent());
+		} else if (rectValue<this.maxBoundaryValue) {
+			if (this.state.progress===0) {
+				if (this.state.scrollState!=='enter') {
+					this.state.scrollState='enter'
+					this._showUpdatePin = false;
+					this.emit('enter');
+				}
+			}
+			if (this.state.scrollState==='enter') {
+				this.state.scrollState='leave'
+				this.state.progress=1;
+				this._showUpdatePin = true;
+				this.emit('progress',this.statetoEvent())
+				this.emit('leave');
+			}
+		}
+		else if (rectValue>minBoundaryValue) {
+			if (this.state.progress === 1) {
+				if (this.state.scrollState!=='enter') {
+					this.state.scrollState='enter'
+					this._showUpdatePin = false;
+					this.emit('enter');
+				}
+			}
+			if (this.state.scrollState==='enter') {
+				this.state.scrollState='leave'
+				this.state.progress=0;
+				this._showUpdatePin = true;
+				this.emit('progress',this.statetoEvent());
+				this.emit('leave');
+			}
+		}
+	}
+	this._lastRectValue=rectValue;
+}
+
+Scene.prototype.setBoundaryValue=function () {
+	this.minBoundaryValue=window.innerHeight*this.Op.triggerHook+this.Op.offset;
+	this.maxBoundaryValue=this.minBoundaryValue-this.Op.duration;
+}
+
+Scene.prototype.addTo=function (controller) {
+	if (!(controller instanceof Controller)) {
+		throw new TypeError('controller must be instance of Controller')
+	}
+	this.update();
+	this.scrollDestory=controller.addScrollEvent(this.Op.container||window,this.update);
+	return this;
+}
+
+Scene.prototype.destory=function () {
+	this._scrollDestory&&this._scrollDestory();
+	this.scrollDestory=null;
+}
+
+Scene.prototype.setPin = function (el) {
+	if(this._isPin) return;
+	this.el = el;
+	this._parentNode = el.parentNode;
+	this._parentNodeStyler = styler(this._parentNode);
+	this._elStyler = styler(this.el);
+	this._pinLeft = getElRect(this._parentNode).left;
+	this.updateParentNode();
+	this.update();
+	this.updatePin();
+	this._isPin = true;
+}
+Scene.prototype.updateParentNode = function() {
+	let elRect = getElRect(this.el);
+	this._parentNodeStyler.set({
+		position: 'relative',
+		height: elRect.height + this.Op.duration,
+	})
+	this._parentNodeStyler.render();
+}
+
+Scene.prototype.updatePin = function() {
+	if(this.Op.duration === 0) {
+		return this.updatePinNoDuration();
+	}
+	this.updatePinWithDuration();
+}
+
+Scene.prototype.updateScene = function(options) {
+
+}
+
+Scene.prototype.removePin = function() {
+	if(!this._isPin) return;
+	this._isPin = false;
+}
+
+Scene.prototype.updatePinNoDuration = function() {
+	let progress = this.state.progress;
+	if(progress === 1) {
+		this._elStyler.set({
+			position: 'fixed',
+			top: this.minBoundaryValue,
+			left: this._pinLeft,
+			right: 'auto',
+			bottom: 'auto',
+			width: getElRect(this._parentNode).width
+		})
+	}else {
+		this._elStyler.set({
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			right: 'auto',
+			bottom: 'auto'
+		})
 	}
 }
 
-Scene({duration: 1,offset: 12})
+Scene.prototype.updatePinWithDuration = function() {
+	let progress = this.state.progress;
+	if(progress === 0) {
+		this._elStyler.set({
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			right: 'auto',
+			bottom: 'auto'
+		})
+	}else if(progress === 1){
+		this._elStyler.set({
+			position: 'absolute',
+			top: 'auto',
+			left: 0,
+			right: 'auto',
+			bottom: 0
+		})
+	}else {
+		this._elStyler.set({
+			position: 'fixed',
+			top: this.minBoundaryValue,
+			left: this._pinLeft,
+			right: 'auto',
+			bottom: 'auto',
+			width: getElRect(this._parentNode).width
+		})
+	}
+}
+
+Scene.prototype.statetoEvent=function () {
+	return Object.assign({},this.state);
+}
+
+Scene.prototype._enter=function () {
+	if(!this._isPin || !this._showUpdatePin) return;
+	this.updatePin();
+}
+Scene.prototype._leave=function () {
+	if(!this._isPin) return;
+	this.updatePin();
+}
+
+export default ScrollMagic;
+export {
+	EventEmitter,
+	styler,
+}
---
+++ myScrollMagic/util.js
@@ -0,0 +1,254 @@
+
+function getPositionTop(el) {
+	if(!el) {
+		console.error('el is null');
+		return;
+	}
+	return getScrollTop() + el.getBoundingClientRect().top;
+}
+
+function getScrollTop() {
+	return document.body.scrollTop || document.documentElement.scrollTop;
+}
+
+function getElRect(el) {
+	if(!el) return;
+	return el.getBoundingClientRect();
+}
+
+function setScrollTop(val) {
+	document.body.scrollTop = document.documentElement.scrollTop = val;
+}
+
+function addEvent(el,type,fn) {
+	if(!el || !type || !fn) return;
+	el.addEventListener(type,fn);
+	return function() {
+		el.removeEventListener(type,fn)
+	}
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: { value: subClass, writable: true, configurable: true }
+  });
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+
+function _setPrototypeOf(o, p) {
+	// eslint-disable-next-line
+  _setPrototypeOf =
+    Object.setPrototypeOf ||
+    function _setPrototypeOf(o, p) {
+      o.__proto__ = p;
+      return o;
+    };
+  return _setPrototypeOf(o, p);
+}
+
+function imageLoader(el) {
+	return new Promise((resolve,reject) => {
+		let imgs = el.getElementsByTagName('img');
+		let complete = 0;
+		let err = false;
+		let len = imgs.length;
+		let count = len;
+		console.log(count)
+		function load() {
+			complete ++;
+				if(complete === count) {
+					if(err) {
+						return reject();
+					}
+					resolve();
+					console.log('onload')
+				}
+		}
+		function error() {
+			complete ++;
+			err = true;
+		}
+		for(let i = 0;i < len; i ++) {
+			let img = imgs[i];
+			if(img.src) {
+				img.onload = load;
+				img.onerror = error;
+				if(getComputedStyle(img).display === 'none') {
+					count --;
+				}
+			}else {
+				count --;
+			}
+		}
+	});
+}
+
+function toUrlUnderline(str) {
+	return str.split(' ').map(s => s.toLowerCase()).join('-')
+}
+
+
+function EventEmiter() {
+	this.listeners = {};
+}
+EventEmiter.prototype = {
+	constructor: EventEmiter,
+	on(type, fn) {
+		if (!type || fn instanceof Array) return;
+		if (!this.listeners[type]) {
+			this.listeners[type] = [];
+		}
+		this.listeners[type].push(fn)
+	},
+	emit(type, ...args) {
+		if (!type || typeof type !== 'string') return;
+		let fns = this.listeners[type];
+		if (!fns instanceof Array) return;
+		fns.forEach(fn => {
+			fn.call(this,{
+				type,
+				data: args.length <= 1 ? args[0]: [...args]
+			}, args);
+		})
+	},
+	remove(type, fn) {
+		if (!type || typeof type !== 'string') return;
+		if (!fn) {
+			this.listeners[type] = [];
+		} else {
+			var fns = this.listeners[type];
+			var len = fns.length;
+			for (var i = len - 1; i >= 0; --i) {
+				var f = fns[i];
+				if (f === fn) {
+					fns.splice(i, 1);
+				}
+			}
+		}
+	}
+}
+
+function getScrollBarWidth() {
+	var noScroll, scroll, oDiv = document.createElement("DIV");
+	oDiv.style.cssText = "position:absolute;top:-1000px;width:100px;height:100px; overflow:hidden;";
+	noScroll = document.body.appendChild(oDiv).clientWidth;
+	oDiv.style.overflowY = "scroll";
+	scroll = oDiv.clientWidth;
+	document.body.removeChild(oDiv);
+	return noScroll-scroll;
+}
+
+// var scroll = (function (deskclassName,mobileClassName) {
+// 	let className = '';
+// 	if(Platfrom.isDesktop) {
+// 		className = deskclassName;
+// 	}else {
+// 		className = mobileClassName;
+// 	}
+// 	var scrollTop;
+// 	var scrollBarWidth = getScrollBarWidth();
+// 	return {
+// 			afterOpen: function () {
+// 				if(Platfrom.isDesktop) {
+// 					document.body.classList.add(className);
+// 					document.body.style.paddingRight = scrollBarWidth + 'px';
+// 				}else {
+// 					scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+// 					document.body.classList.add(className);
+// 					document.body.style.top = -scrollTop + 'px';
+// 				}
+// 			},
+// 			beforeClose: function () {
+// 				if(Platfrom.isDesktop) {
+// 					document.body.classList.remove(className);
+// 					document.body.removeAttribute('style');
+// 				}else {
+// 					document.body.classList.remove(className);
+// 					document.body.removeAttribute('style');
+// 					window.scrollTo(0,scrollTop);
+// 				}
+// 			}
+// 	};
+// })('overflow','positionFixed');
+
+
+var urlRex = {
+	protocol: /([^/]+:)\/\/(.*)/i,
+	host: /(^[^:/]+)((?:\/|:|$)?.*)/,
+	port: /:?([^/]*)(\/?.*)/,
+	pathname: /([^?#]+)(\??[^#]*)(#?.*)/
+};
+
+function parseUrl(url) {
+	var tmp, res = {};
+	res["href"] = url;
+	for (let p in urlRex) {
+			tmp = urlRex[p].exec(url);
+			res[p] = tmp[1];
+			url = tmp[2];
+			if (url === "") {
+					url = "/";
+			}
+			if (p === "pathname") {
+					res["pathname"] = tmp[1];
+					res["search"] = tmp[2];
+					res["hash"] = tmp[3];
+			}
+	}
+	return res;
+};
+
+
+function nowTime() {
+	return new Date().getTime();
+}
+function throttle(func, wait, options) {
+	var context, args, result;
+	var timeout = null;
+	var previous = 0;
+	if (!options) options = {};
+	var later = function() {
+		previous = options.leading === false ? 0 : nowTime();
+		timeout = null;
+		result = func.apply(context, args);
+		if (!timeout) context = args = null;
+	};
+	return function() {
+		var now = nowTime();
+		if (!previous && options.leading === false) previous = now;
+		var remaining = wait - (now - previous);
+		context = this;
+		args = arguments;
+		if (remaining <= 0 || remaining > wait) {
+			clearTimeout(timeout);
+			timeout = null;
+			previous = now;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		} else if (!timeout && options.trailing !== false) {
+			timeout = setTimeout(later, remaining);
+		}
+		return result;
+	};
+};
+
+
+export { 
+	getPositionTop,
+	getScrollTop,
+	setScrollTop,
+	imageLoader,
+	getElRect,
+	addEvent,
+	toUrlUnderline,
+	EventEmiter,
+	// scroll,
+	parseUrl,
+	throttle,
+	_inherits,
+	getScrollBarWidth
+};
